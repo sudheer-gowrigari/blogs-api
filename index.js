62105fd2b316d421901154481aaab8799630b74b
Sudheer Kumar Reddy Gowrigari <skumarreddy@salesforce.com>
2021-01-17 14:49:48 +0530

    production ready

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const {pool} = require('./config')
-//const helmet = require('helmet')
-//const compression = require('compression')
+const helmet = require('helmet')
+const compression = require('compression')
 const rateLimit = require('express-rate-limit')
 const {body, check} = require('express-validator')
 
@@ -12,8 +12,8 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
-//app.use(compression())
-//app.use(helmet())
+app.use(compression())
+app.use(helmet())
 const isProduction = process.env.NODE_ENV === 'production'
 const origin = {
   origin: isProduction ? 'https://salesforce-blogs.herokuapp.com/' : '*',
