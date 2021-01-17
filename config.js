a56664f99a558d02affd50fbbde591800f3c5d1d
Sudheer Kumar Reddy Gowrigari <skumarreddy@salesforce.com>
2021-01-17 14:58:34 +0530

    fixing unauthorised certificate issue

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const {Pool} = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
